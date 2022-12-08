5c14a4d033899f85e7f0d639268443a66dd1ffed
Josh M Thomas <71394743+Josh236061@users.noreply.github.com>
2022-12-07 21:59:25 -0600

    Fixed Lesson 1 | Initialized Lesson 2 Code

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
