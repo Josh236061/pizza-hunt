4e4635e68498d5114f5788af9f75161b1fb9fb25
Josh236061 <71394743+Josh236061@users.noreply.github.com>
2022-12-12 15:43:17 -0600

    Deploying

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   }
 );
 
